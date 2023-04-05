fa9d74db82afa462527107571f978085c0ab339e
Ilya <admin@redguy.ru>
2023-04-05 21:59:15 +0300

    Bearer Authorization middleware

---
+++ src/main/java/ru/redguy/jrweb/presets/BearerAuthorizationMiddleware.java
@@ -0,0 +1,57 @@
+package ru.redguy.jrweb.presets;
+
+import org.jetbrains.annotations.NotNull;
+import ru.redguy.jrweb.utils.*;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class BearerAuthorizationMiddleware extends Middleware {
+
+    private final AuthorizationChecker checker;
+
+    public BearerAuthorizationMiddleware(AuthorizationChecker checker) {
+        this(null, checker);
+    }
+
+    public BearerAuthorizationMiddleware(Method method, AuthorizationChecker checker) {
+        super(method);
+        this.checker = checker;
+    }
+
+    @Override
+    public void run(Context ctx) throws IOException {
+        HeaderValue headerValue = ctx.request.headers.getFirst(Headers.Request.AUTHORIZATION);
+        if (headerValue == null) {
+            restrict(ctx);
+            return;
+        }
+        String value = headerValue.getValue();
+        if (value == null) {
+            restrict(ctx);
+            return;
+        }
+        String[] split = value.split(" ");
+        if (split.length != 2) {
+            restrict(ctx);
+            return;
+        }
+        if (!split[0].equals("Bearer")) {
+            restrict(ctx);
+            return;
+        }
+        if (!checker.check(split[1])) {
+            restrict(ctx);
+        }
+    }
+
+    private void restrict(@NotNull Context ctx) {
+        ctx.response.setStatusCode(StatusCodes.UNAUTHORIZED);
+        ctx.cancelled = true;
+        ctx.response.send("Unauthorized");
+    }
+
+    public interface AuthorizationChecker {
+        boolean check(String token);
+    }
+}
