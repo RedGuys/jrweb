ef88ab430a15d5fa819f7a81371e3b9c1c01a772
Ilya <admin@redguy.ru>
2023-07-02 23:43:01 +0300

    Fix error

--- src/main/java/ru/redguy/jrweb/utils/Page.java
+++ src/main/java/ru/redguy/jrweb/utils/Page.java
@@ -22,8 +22,10 @@ public abstract class Page {
 
     public void processRequest(String path,@NotNull Context context) throws IOException {
         if(regex.matcher(path).matches()&& (method == null || method.equals(context.request.method))) {
-            if(BodyParser.bodyParsers.containsKey(context.request.headers.getFirst(Headers.Request.CONTENT_TYPE).getValue())) {
-                BodyParser.bodyParsers.get(context.request.headers.getFirst(Headers.Request.CONTENT_TYPE).getValue()).parse(context);
+            if(context.request.headers.has(Headers.Request.CONTENT_TYPE)) {
+                if (BodyParser.bodyParsers.containsKey(context.request.headers.getFirst(Headers.Request.CONTENT_TYPE).getValue())) {
+                    BodyParser.bodyParsers.get(context.request.headers.getFirst(Headers.Request.CONTENT_TYPE).getValue()).parse(context);
+                }
             }
             run(context);
             context.processed = true;
