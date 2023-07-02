aa4decb3666b783b854cdcea9e3aa55c92c197db
Ilya <admin@redguy.ru>
2023-07-02 23:18:58 +0300

    Fix url encoding

--- src/main/java/ru/redguy/jrweb/utils/bodyparsers/URLEncodedBodyParser.java
+++ src/main/java/ru/redguy/jrweb/utils/bodyparsers/URLEncodedBodyParser.java
@@ -4,6 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import ru.redguy.jrweb.Context;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.HashMap;
 
 public class URLEncodedBodyParser extends BodyParser {
@@ -24,7 +25,7 @@ public class URLEncodedBodyParser extends BodyParser {
             HashMap<String, String> parameters = new HashMap<>();
             for (String param : params) {
                 String[] keyValue = param.split("=");
-                parameters.put(keyValue[0], keyValue[1]);
+                parameters.put(URLDecoder.decode(keyValue[0], "UTF-8"), URLDecoder.decode(keyValue[1], "UTF-8"));
             }
             context.request.params = parameters;
         } catch (IOException e) {
