ffff8c171436e408665caf4498391e55903ac909
Ilya <admin@redguy.ru>
2024-06-27 18:55:59 +0300

    Release 2.0.6 - Allow to get registered GsonUtil type adaptors

--- src/main/java/ru/redguy/jrweb/utils/optional/GsonUtil.java
+++ src/main/java/ru/redguy/jrweb/utils/optional/GsonUtil.java
@@ -4,9 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
+import java.util.HashMap;
+
 public class GsonUtil {
 
     private static Gson gson = new Gson();
+    private static final HashMap<Class<?>,Object> typeAdapters = new HashMap<>();
 
     public static Gson getGson() {
         return gson;
@@ -14,6 +17,15 @@ public class GsonUtil {
 
     public static void registerTypeAdapter(Class<?> type, Object typeAdapter) {
         gson = gson.newBuilder().registerTypeAdapter(type, typeAdapter).create();
+        typeAdapters.put(type, typeAdapter);
+    }
+
+    public static boolean isTypeAdapterRegistered(Class<?> type) {
+        return typeAdapters.containsKey(type);
+    }
+
+    public static Object getTypeAdapter(Class<?> type) {
+        return typeAdapters.get(type);
     }
 
     public static JsonElement parse(String json) {
